46e0b2ee4cb535bb743c449e60ef58c5a1bedba2
Jimmy Song <jimcsong@gmail.com>
2020-05-17 13:20:39 -0700

    add ping endpoint

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,5 +1,11 @@
 class CommandsController < ApplicationController
 
+  skip_before_action :verify_slack_signature, only: :ping
+
+  def ping
+    render json: {status: 'ok'}
+  end
+
   def run
     Rails.logger.warn(params)
     head :ok
